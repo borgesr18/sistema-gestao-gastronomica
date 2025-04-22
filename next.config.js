49a0d4259a80e8b4462489318c9b01ed44f41def
borgesr18 <borgesr18@yahoo.com.br>
2025-04-22 08:07:44 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,15 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  images: {
-    unoptimized: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
+  images: { unoptimized: true }
 };
 
 module.exports = nextConfig;
