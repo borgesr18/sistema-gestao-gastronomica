d008b9aafc14b891e7f2241c4f77bc5e295414df
borgesr18 <borgesr18@yahoo.com.br>
2025-04-22 07:42:19 -0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,15 @@
-import { NextConfig } from 'next'
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
   eslint: {
     ignoreDuringBuilds: true,
   },
   typescript: {
     ignoreBuildErrors: true,
   },
-}
+};
 
-export default nextConfig
+module.exports = nextConfig;
